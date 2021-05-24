33a2884c85b1cd7969f1139d07be85bb2878d321
Eclipsu <stharj219@gmail.com>
2021-05-24 19:25:23 +0545

    Optimized code Repalced all getters and setts to one function that returns a tuple

--- src/BackEnd/app.py
+++ src/BackEnd/app.py
@@ -1,100 +1,75 @@
 import os 
 import shutil
 import json
+import sys
 
-def write_to_logs(log_msg):
+def getMetadata():
     """
-    Writes to logs to double check errors and mistakes   
+    Gets details about the dir.
     """
-    with open("log.txt", w) as log_file:
-        log_file.append(log_msg)
-
-
-def delete(path):
-    """Deletes every file in a folder
-    path: folder path
-    """
-    for f in os.listdir(path):
-        os.remove(os.path.join(path, f))
-
-def getPath():
-    """
-    Gets path of the from ./metadata.json/  
-    """
-    with open('metadata.json', 'r') as openfile:
-        global path
-        json_object = json.load(openfile)
-        pairs = json_object.items()
-        path = json_object["renamer"]["path"]
-    return path
-
-def getStartNumber():
-    """
-    returns the number of start renaming from ./metadata.json
-    """
-    with open('metadata.json', 'r') as openfile:
-        json_object = json.load(openfile)
-        pairs = json_object.items()
-        srt = json_object["renamer"]["srt"]
-    return srt
-
-def getSrcDst():
-    """
-    gets Sorce and Destination path from ./metadata.json/
-    """
-    with open('metadata.json', 'r') as openfile:
-        global path
-        json_object = json.load(openfile)
-        pairs = json_object.items()
-        src = json_object["backup"]["src"]
-        dst = json_object["backup"]["dst"]
-        srt = json_object["renamer"]["srt"]
-        return src, dst
+    with open('metadata.json', 'r') as metadata_file:
+        metadata = json.load(metadata_file)
+        starting_number = metadata["details"]["starting_number"]
+        backup_path = metadata["details"]["dir_path"]
+        dir_path = metadata["details"]["backup_path"]
+    return starting_number, backup_path, dir_path
 
 def backup(symlinks=False, ignore=None):
     """
     Backs up Renaming folder to a new folder in Desktop to prevent loss from errors
     """
-    (src, dst) = getSrcDst()
-    delete(dst)
+    (starting_number, backup_path, dir_path) = getMetadata()
+
+    if dir_path == '':
+        print("Invalid path")
+        exit()
+    if backup_path == '':
+        print("No backup specified")
+        exit()
+
     try:
-        for item in os.listdir(src):
-            s = os.path.join(src, item)
-            d = os.path.join(dst, item)
+        for item in os.listdir(dir_path):
+            s = os.path.join(dir_path, item)
+            d = os.path.join(backup_path, item)
             if os.path.isdir(s):
                 shutil.copytree(s, d, symlinks, ignore)
             else:
                 shutil.copy2(s, d)
-                # print(f"Back up'd {src} in {dst}!")
     except:
-        print("failed")
+        print("Failed to backup")
 
 def rename():
     """
     renames the renaming folder from according to the starting number 
     """
-    srt = getStartNumber()
-    path, backup_path = getSrcDst()
+    (starting_number, backup_path, dir_path) = getMetadata()
+    
+    
+    if dir_path == '':
+        print("Invalid path")
+        exit()
+    if backup_path == '':
+        print("No backup specified")
+        exit()
+
+    filename, file_extension = os.path.splitext('/path/to/somefile.ext')
+
+
     backup()
-    files = os.listdir(path) 
+    files = os.listdir(dir_path) 
     loop_number = 0
+
     for index, file in enumerate(files):
-        
-        if not file.endswith('.jpg'):
-            if not file.endswith('.JPG'):
-                index = index - 1
-                continue
-                skips += 1
         try:
-            number = srt + loop_number
+            number = starting_number + loop_number
             loop_number +=  1
-            os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(number), '.jpg'])))
-            write_to_logs(f"{file} → {number}")
+            filename, file_extension = os.path.splitext(file)
+            os.rename(os.path.join(dir_path, file), os.path.join(dir_path, ''.join([str(number), file_extension])))
             print(f"{file} → {number}")
-            sucess += 1
         except Exception as err:
-            print(err)
+            print("Failed to rename")
 
 backup()
-
 rename()
+
+print("Sucess!")
