369e3f4acf57f58edf1ca4498415f6e214bbd7e3
Eclipsu <stharj219@gmail.com>
2021-05-24 19:26:16 +0545

    Error handling

--- src/BackEnd/index.js
+++ src/BackEnd/index.js
@@ -1,4 +1,5 @@
 const {PythonShell} = require('python-shell')
+const path = require("path")
 
 var bleep = new Audio();
 bleep.src = "../../assets/click sound.mp3";
@@ -6,16 +7,20 @@ bleep.src = "../../assets/click sound.mp3";
 const renameButon = document.getElementById("rename-button");
 
 renameButon.addEventListener('click', () => {
-
     let loader = document.getElementById("loader");
+    loader.classList.replace("hidden", "visible");
     try {
-        PythonShell.run("app.py", null, (err) => {
-            loader.classList.replace("hidden", "visible");
+        PythonShell.run("/home/eclipsu/Documents/WorkSpace/pariwartan/src/BackEnd/app.py", null, (err, respnse_message) => {
             if (err) {
                 alert(`${err}`);
+                loader.classList.replace("visible", "hidden");
+
             }
             else {
-                loader.classList.replace("hidden", "visible");
+
+                alert(respnse_message)
+                loader.classList.replace("visible", "hidden")
+               
             }
         })
     }
