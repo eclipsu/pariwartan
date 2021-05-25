b4f851bda8803264c621140919851961e692bebc
Eclipsu <stharj219@gmail.com>
2021-05-25 17:17:27 +0545

    Sweet alert Added sweet alert for sweet alerts Fixed background repeat

--- src/BackEnd/index.js
+++ src/BackEnd/index.js
@@ -1,31 +1,49 @@
 const {PythonShell} = require('python-shell')
 const path = require("path")
+const Swal = require('sweetalert2')
+
 
 var bleep = new Audio();
 bleep.src = "../../assets/click sound.mp3";
 
 const renameButon = document.getElementById("rename-button");
 
-renameButon.addEventListener('click', () => {
+renameButon.addEventListener('click', () => {   
     let loader = document.getElementById("loader");
     loader.classList.replace("hidden", "visible");
     try {
         PythonShell.run("/home/eclipsu/Documents/WorkSpace/pariwartan/src/BackEnd/app.py", null, (err, respnse_message) => {
             if (err) {
-                alert(`${err}`);
+                Swal.fire({
+                    title: `${err}`,
+                    showConfirmButton: true,
+                    width: '850px',
+                    height: '750px',
+                 });
                 loader.classList.replace("visible", "hidden");
 
             }
             else {
 
-                alert(respnse_message)
+                Swal.fire({
+                    title: `${respnse_message}`,
+                    customClass: 'swal-wide',
+                    showConfirmButton: true,
+                    width: '850px',
+                    height: '750px',
+                 });
                 loader.classList.replace("visible", "hidden")
                
             }
         })
     }
     catch(err) {
-        alert(`${err}`);
+        Swal.fire({
+            title: `${err}`,
+            showConfirmButton: true,
+            width: '850px',
+            height: '750px',
+         });
     }
 
 })
