8aa56e566c466dca91b1137ae4c65978a08e03d3
Eclipsu <stharj219@gmail.com>
2021-05-25 23:04:04 +0545

    Fixed bug for linux, optimized code it loops through every file in the folder
    but the problem was it was scrambled
    
    Then i made a empty list called unsorted_files
    And, for every file in the folder: unsorted_files.append()
    
    then i found python had awesome function to sort list of any types
    unsorted_files.sort()
    
    and now the files are sorted
    
    all i have to do now is irritate through the sorted list and use  use os.rename

--- src/BackEnd/app.py
+++ src/BackEnd/app.py
@@ -1,74 +1,39 @@
 import os 
-import shutil
 import json
-import sys
 
-def getMetadata():
-    """
-    Gets details about the dir.
-    """
-    with open('metadata.json', 'r') as metadata_file:
-        metadata = json.load(metadata_file)
-        starting_number = metadata["details"]["starting_number"]
-        backup_path = metadata["details"]["dir_path"]
-        dir_path = metadata["details"]["backup_path"]
-    return starting_number, backup_path, dir_path
 
-def backup(symlinks=False, ignore=None):
-    """
-    Backs up Renaming folder to a new folder in Desktop to prevent loss from errors
-    """
-    (starting_number, backup_path, dir_path) = getMetadata()
+metadata_file = open('metadata.json', 'r')
 
-    if dir_path == '':
-        print("Invalid path")
-        exit()
-    if backup_path == '':
-        print("No backup specified")
-        exit()
+metadata = json.load(metadata_file)
 
-    try:
-        for item in os.listdir(dir_path):
-            s = os.path.join(dir_path, item)
-            d = os.path.join(backup_path, item)
-            if os.path.isdir(s):
-                shutil.copytree(s, d, symlinks, ignore)
-            else:
-                shutil.copy2(s, d)
-    except:
-        pass
+starting_number = metadata["details"]["starting_number"]
+backup_path = metadata["details"]["dir_path"]
+dir_path = metadata["details"]["backup_path"]
+
+metadata_file.close()
+
     
-def rename():
+def rename(starting_number, dir_path):
     """
     renames the renaming folder from according to the starting number 
     """
-    (starting_number, backup_path, dir_path) = getMetadata()
 
-
-    if dir_path == '':
-        print("Invalid path")
-        exit()
-    if backup_path == '':
-        print("No backup specified")
-        exit()
-
-
+    files_list = []
 
     files = os.listdir(dir_path) 
-    loop_number = 0
     for index, file in enumerate(files):
-        print("lol")
-        filename, file_extension = os.path.splitext(os.path.join(dir_path, file))
-        try:
-            number = starting_number + loop_number
-            loop_number +=  1
-            filename, file_extension = os.path.splitext(file)
-            os.rename(os.path.join(dir_path, file), os.path.join(dir_path, ''.join([str(number), file_extension])))
-            print(f"{file} → {number}")
-        except Exception as err:
-            print("Failed to rename")
+        filename, file_extension = os.path.splitext(file)
+        files_list.append(filename + file_extension)
 
-backup()
-rename()
+    files_list.sort()
+
+    for file in files_list:
+        filename, file_extension = os.path.splitext(file)
+        os.rename(os.path.join(dir_path, file), os.path.join(dir_path, ''.join([str(starting_number), f"{file_extension}"])))
+
+        print(f"{file} ---> {starting_number}{file_extension}")
+        starting_number += 1
+
+rename(starting_number, dir_path)
 
 print("Sucess!")
