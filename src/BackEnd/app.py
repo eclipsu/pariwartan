1de8a81538f0aa6e8e6a8162579dcecbfeda5690
Eclipsu <stharj219@gmail.com>
2021-05-26 14:58:05 +0545

    Fixed bugs Added error handlers New metadata formats since we don't need backup path anymore

--- src/BackEnd/app.py
+++ src/BackEnd/app.py
@@ -6,20 +6,25 @@ metadata_file = open('metadata.json', 'r')
 
 metadata = json.load(metadata_file)
 
-starting_number = metadata["details"]["starting_number"]
-backup_path = metadata["details"]["dir_path"]
-dir_path = metadata["details"]["backup_path"]
+starting_number = metadata["starting_number"]
+dir_path = metadata["dir_path"]
 
 metadata_file.close()
 
-    
+if type(starting_number) != int or starting_number < 0:
+    print("Invalid number!") 
+    quit()
+
+if dir_path == "":
+    print("Invalid path!")
+    quit()
+
 def rename(starting_number, dir_path):
     """
     renames the renaming folder from according to the starting number 
     """
 
     files_list = []
-
     files = os.listdir(dir_path) 
     for index, file in enumerate(files):
         filename, file_extension = os.path.splitext(file)
@@ -31,7 +36,6 @@ def rename(starting_number, dir_path):
         filename, file_extension = os.path.splitext(file)
         os.rename(os.path.join(dir_path, file), os.path.join(dir_path, ''.join([str(starting_number), f"{file_extension}"])))
 
-        print(f"{file} ---> {starting_number}{file_extension}")
         starting_number += 1
 
 rename(starting_number, dir_path)
