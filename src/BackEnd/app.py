0cc318373a79cc755425a572a9aa83ed5e5b5251
Eclipsu <stharj219@gmail.com>
2021-05-27 20:23:01 +0545

    Fixed ValueError Fixed ValueError: invalid literal for int() with base 10: 'A' Will ignore old files, only renames newly added files. Optimized, I AM SPEED.

--- src/BackEnd/app.py
+++ src/BackEnd/app.py
@@ -1,9 +1,7 @@
 import os 
 import json
 
-
 metadata_file = open('metadata.json', 'r')
-
 metadata = json.load(metadata_file)
 
 starting_number = metadata["starting_number"]
@@ -23,17 +21,24 @@ def rename(starting_number, dir_path):
     """
     renames the renaming folder from according to the starting number 
     """
+    numeric_files = [] # Older files, Files which we want to ignore.
+    strings_files = [] # Newer files, Files which we want to rename.
 
-    files = os.listdir(dir_path)
+    for file in os.listdir(dir_path):
+        try:
+            int_file = int(os.path.splitext(file)[0])
+            numeric_files.append(file)
+        except:
+            strings_files.append(file)
 
-    sorted_file = sorted(files, key = lambda x: int(os.path.splitext(x)[0]))
-
-    for file in sorted_file:
-        filename, file_extension = os.path.splitext(file)
-        os.rename(os.path.join(dir_path, file), os.path.join(dir_path, ''.join([str(starting_number), f"{file_extension}"])))
-
-        # print(f"{file} -->  {os.path.join(''.join([str(starting_number), f'{file_extension}']))}")
+    for files in numeric_files:
         starting_number += 1
+
+    for files in strings_files:
+        starting_number += 1
+        filename, file_extension = os.path.splitext(files)
+        os.rename(os.path.join(dir_path, files), os.path.join(dir_path, ''.join([str(starting_number), f"{file_extension}"])))
+        # print(f"{files} -->  {os.path.join(''.join([str(starting_number), f'{file_extension}']))}")
 
 rename(starting_number, dir_path)
 
